9c0e9ee5da15572d21f19d7b44384c94956cfdb5
dfjj <dfjjfcdsgnkh@gmail.com>
2025-08-22 18:27:49 +0900

    feat:c++で実装#1

---
+++ build/CMakeFiles/realsenseclass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realsenseclass.dir/realsense.cpp.o"
+  "CMakeFiles/realsenseclass.dir/realsense.cpp.o.d"
+  "librealsenseclass.a"
+  "librealsenseclass.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realsenseclass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
